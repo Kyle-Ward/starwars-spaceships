9e2421850b06645c612481f97c53e226e252796c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-31 20:35:38 +0000

    Deploy zero-to-mastery/starwars-spaceships to github.com/zero-to-mastery/starwars-spaceships.git:gh-pages

---
+++ precache-manifest.7d8802abff7f7b87c9f852bf129ab2fe.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "738b24867567c2613cc7",
+    "url": "./static/css/main.9cb6a114.chunk.css"
+  },
+  {
+    "revision": "738b24867567c2613cc7",
+    "url": "./static/js/main.c87b3581.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3da6a664b61153997577",
+    "url": "./static/css/2.6aff6454.chunk.css"
+  },
+  {
+    "revision": "3da6a664b61153997577",
+    "url": "./static/js/2.2042fda3.chunk.js"
+  },
+  {
+    "revision": "9b9887610c0940a8ca92816b10175038",
+    "url": "./static/media/Alderaan.9b988761.jpg"
+  },
+  {
+    "revision": "58795608d50457c340e76d763e73c8d5",
+    "url": "./static/media/Dagobah.58795608.jpg"
+  },
+  {
+    "revision": "586f2ab18e7ac9bf11a807b2516cd2a7",
+    "url": "./static/media/Kamino.586f2ab1.jpg"
+  },
+  {
+    "revision": "3b8cdd01a8247ded16023eae17035fe1",
+    "url": "./static/media/Bespin.3b8cdd01.png"
+  },
+  {
+    "revision": "567399d64f0ee2d147548cbf804d2b87",
+    "url": "./static/media/Coruscant.567399d6.png"
+  },
+  {
+    "revision": "6c79084ffa275b864f276893d68de5f1",
+    "url": "./static/media/Endor.6c79084f.png"
+  },
+  {
+    "revision": "21d0556357edac975ae628bf4a27cc86",
+    "url": "./static/media/Hoth.21d05563.png"
+  },
+  {
+    "revision": "573010fdcda05da83f741134099f4311",
+    "url": "./static/media/Naboo.573010fd.png"
+  },
+  {
+    "revision": "574e698c5f254383433fdfbb94d1fc15",
+    "url": "./static/media/Yavin IV.574e698c.png"
+  },
+  {
+    "revision": "c77e09ae4e2d1f9d925bebdfe1f239c0",
+    "url": "./static/media/spaceship.c77e09ae.png"
+  },
+  {
+    "revision": "587661e16ba2ddb28f20014bc782e4e5",
+    "url": "./index.html"
+  }
+];
